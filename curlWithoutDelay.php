02319d3c0b39f3af83646245a9b49a82735285e3
SOLOMON ADDAI-BOAKYE <117929232+sabtechhub@users.noreply.github.com>
2024-10-08 15:20:00 -0400

    Update curlWithoutDelay.php

--- curlWithoutDelay.php
+++ curlWithoutDelay.php
@@ -33,7 +33,7 @@ class JunipayService
             CURLOPT_HTTPHEADER => [
                 'clientid: Qys10304',
                 'Content-Type: application/json',
-                'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiIiwiaWF0IjoxNjY1MDkzMjQxfQ.pRFO8MCG06NkeXJ9mCefTi90yMKLu8tdiEUvLlxiWl4WkgfygXwCdrqs5aywDOwAL_7QQXv5Mul7_ASBZS25g-klBUBUMi6yw4HPw6SBLv-Q0ktoSCjIRWlL3N9-V-bHhndn1qAD2wU4HShhtFhT8gxoaNj5IvWv_upEdRMky2f6Ky6P_rZuv5wVMn_9QjfDeGuRnPcvrbwXO0k_1kGGhYBjozeOyWARhUUjwIAjIBH5u1SQATSpFEs20p308NeUeumiluaMve8MsVORP_bAVDkKM2ZMVvH609AUDrVmns-sFqoiwx9tl-nztfq8n_VJyaVmJQ3uJDSDQ1LKFWT3kicyJBl-Cg4QzFt0DggEu0WyWPWp311CBohLxp6A58bSuNkg5wwCP_Rfa_0Vh2PsJvZpYiLoGhTU1YnjCcspRiBAC6ijAcMfmdDbwRWvzSUzFmjXFFlJqxImbvetPI_kqunNQGHFVEfe9DgDWZAWfneReW4rJ5xPivEe28J0qJteDcqDgu9sET9n4oTmHmXfqlwpIz4ciPxhPdx2fWFAUomE0MSrG_n_wnHTlCqHtvc-xDJOSiSiwKkwGp7XfsgmPwtTb9DY0OwSKdgwY8s0ZJEUkUQ6TG66emZJPdxL4uH7g_fN2gJJuhUKBrIHiFeHjSs_gz5yID-7ewBlzOxq1X0',
+                'Authorization: Bearer '.$token,
             ]
         ]);
 
